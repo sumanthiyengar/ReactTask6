1dbbed87109201f0bc588300f0b92cc710a38e74
Sumanth Iyengar <sumanthiyengar2003@gmail.com>
2023-10-16 18:37:09 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => (
+  <section>
+    <h2>Contact Me</h2>
+    <p>Email: sumanth,mn@gmail.com</p>
+    <p>LinkedIn: https://www.linkedin.com/in/sumanth-narayan-788689244/</p>
+  </section>
+);
+
+export default Contact;
